efec5453355d1fbfe56eedc381780ffc3bc2493b
Kyohei Seo <kyo199333@gmail.com>
2015-11-21 15:49:38 +0900

    質問に関するViewやViewControllerを作成した。

---
+++ PocketGuide/QuestionView.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionView.swift
+//  PocketGuide
+//
+//  Created by Seo Kyohei on 2015/11/21.
+//  Copyright © 2015年 Kyohei Seo. All rights reserved.
+//
+
+import UIKit
+
+class QuestionView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
