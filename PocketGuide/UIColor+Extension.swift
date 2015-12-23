f0eca1ff47ba91de5658dce01052c251fbf6099c
Kyohei Seo <kyo199333@gmail.com>
2015-12-24 02:01:36 +0900

    add extension file of UIColor

---
+++ PocketGuide/UIColor+Extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIColor+Extension.swift
+//  PocketGuide
+//
+//  Created by Seo Kyohei on 2015/12/23.
+//  Copyright © 2015年 Kyohei Seo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    
+    class func rgb(r r: Int, g: Int, b: Int, alpha: CGFloat) -> UIColor{
+        return UIColor(red: CGFloat(r) / 255.0, green: CGFloat(g) / 255.0, blue: CGFloat(b) / 255.0, alpha: alpha)
+    }
+    
+    class func barColor()-> UIColor {
+        return UIColor.rgb(r: 116, g: 168, b: 71, alpha: 1.0)
+    }
+    
+}
